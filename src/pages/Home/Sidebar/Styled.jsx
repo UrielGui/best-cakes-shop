d1c1ac2691836d33cc1f77d4c75f75a68cc67f72
Uriel Guimarães <urieligara@hotmail.com>
2021-06-12 01:24:02 -0300

    sidebar styled

--- src/pages/Home/Sidebar/Styled.jsx
+++ src/pages/Home/Sidebar/Styled.jsx
@@ -1,16 +1,39 @@
 import styled from 'styled-components';
 
 export const Sidebar = styled.section`
-    background-color: azure;
-    margin-top: 20px;
+
 `
 
 export const SidebarContainer = styled.div`
-    padding: 10px;
+    background-color: azure;
+    margin-top: 20px;
+    margin-left: 20px;
+    border-radius: 20px;
+`
+
+export const SidebarRow = styled.div`
+    margin-left: 10px;
+`
+
+export const SideBarTitleContainer = styled.div`
+    padding-top: 20px;
+    padding-bottom: 20px;
+    background-color: #3a0f2e;
+    margin-bottom: 10px;
+    border-radius: 20px 20px 0px 0px;
+`
+
+export const SidebarContainerItem = styled.div`
+    margin-left: 10px;
+    padding-bottom: 10px;
 `
 
 export const FilterTextClick = styled.span `
     cursor: pointer;
     font-size: 12px;
+`
+
+export const SideBarTitle = styled.h4`
+    color: #fff;    
 
 `
--- src/pages/Home/Sidebar/index.jsx
+++ src/pages/Home/Sidebar/index.jsx
@@ -12,37 +12,47 @@ export default function Sidebar() {
     let { sortOption, setSortOption, sortOptionValue1, setSortOptionValue1, sortOptionValue2, setSortOptionValue2 } = useContext(ProductSortOption)
 
     return (
-          <Styled.Sidebar>
-            <Styled.SidebarContainer>
-              <h4>Ordem de exibição</h4><br></br>
+      <Styled.Sidebar>
+          <Styled.SidebarContainer>
+              <Styled.SideBarTitleContainer>
+              <Styled.SidebarRow>
+                <Styled.SideBarTitle>
+                  Ordem de exibição
+                </Styled.SideBarTitle>
+                </Styled.SidebarRow>
+              </Styled.SideBarTitleContainer>
+
+        <Styled.SidebarContainerItem>
             <Styled.FilterTextClick onClick={() => {
-          setSortOption(sortOption = "price")
-          setSortOptionValue1(sortOptionValue1 = 1);
-          setSortOptionValue2(sortOptionValue2 = -1)
-        }}><AiOutlineArrowDown /> Mais Barato</Styled.FilterTextClick>
+            setSortOption(sortOption = "price")
+            setSortOptionValue1(sortOptionValue1 = 1);
+            setSortOptionValue2(sortOptionValue2 = -1)
+          }}><AiOutlineArrowDown /> Mais Barato</Styled.FilterTextClick>
+        </Styled.SidebarContainerItem>
 
-        <br></br><br></br>
-        <Styled.FilterTextClick onClick={() => {
-          setSortOption(sortOption = "price")
-          setSortOptionValue1(sortOptionValue1 = -1);
-          setSortOptionValue2(sortOptionValue2 = 1)
-        }}><AiOutlineArrowUp /> Mais Caro</Styled.FilterTextClick>
-        
-        <br></br><br></br>
+        <Styled.SidebarContainerItem>
+          <Styled.FilterTextClick onClick={() => {
+            setSortOption(sortOption = "price")
+            setSortOptionValue1(sortOptionValue1 = -1);
+            setSortOptionValue2(sortOptionValue2 = 1)
+          }}><AiOutlineArrowUp /> Mais Caro</Styled.FilterTextClick>
+        </Styled.SidebarContainerItem>
 
-        <Styled.FilterTextClick onClick={() => {
-          setSortOption(sortOption = "name")
-          setSortOptionValue1(sortOptionValue1 = 1);
-          setSortOptionValue2(sortOptionValue2 = -1)
-        }}><AiOutlineArrowRight /> Ordem Crescente</Styled.FilterTextClick>
+        <Styled.SidebarContainerItem>
+          <Styled.FilterTextClick onClick={() => {
+            setSortOption(sortOption = "name")
+            setSortOptionValue1(sortOptionValue1 = 1);
+            setSortOptionValue2(sortOptionValue2 = -1)
+          }}><AiOutlineArrowRight /> Ordem Crescente</Styled.FilterTextClick>
+        </Styled.SidebarContainerItem>
 
-        <br></br><br></br>
-
-        <Styled.FilterTextClick onClick={() => {
-          setSortOption(sortOption = "name")
-          setSortOptionValue1(sortOptionValue1 = -1);
-          setSortOptionValue2(sortOptionValue2 = 1)
-        }}><AiOutlineArrowLeft /> Ordem Decrescente</Styled.FilterTextClick>
+        <Styled.SidebarContainerItem>
+          <Styled.FilterTextClick onClick={() => {
+            setSortOption(sortOption = "name")
+            setSortOptionValue1(sortOptionValue1 = -1);
+            setSortOptionValue2(sortOptionValue2 = 1)
+          }}><AiOutlineArrowLeft /> Ordem Decrescente</Styled.FilterTextClick>
+          </Styled.SidebarContainerItem>
         </Styled.SidebarContainer>
         </Styled.Sidebar>
 );
